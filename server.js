e223002518ff98b71ddef20e3ce4c66e7e262b36
Katie Pestotnik <katie.pestotnik@gmail.com>
2021-11-10 19:32:25 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const { PORT = 3000 } = process.env;
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
